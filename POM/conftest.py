d253ba34c7aacd9e6272a9fc4a3b454d23b4a3b9
nastassia rabkina <nastassiarabkina@nastassias-MacBook-Air.local>
2023-06-03 15:05:05 +0200

    Relative path to config.json

--- POM/conftest.py
+++ POM/conftest.py
@@ -1,12 +1,19 @@
 import pytest
 from selenium import webdriver
 import json
+import os
+
+# Get the directory of this script
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+# Build the file path for config.json
+config_path = os.path.join(dir_path, 'config.json')
 
 url = 'https://the-internet.herokuapp.com'
 
 @pytest.fixture
 def get_config():
-    with open('config.json') as config_file:
+    with open(config_path) as config_file:
         data = json.load(config_file)
     return data
 
